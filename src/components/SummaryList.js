5d7770d03b86599703cee8c2a18c1cbb13d50bed
Irfan Zainudin <m.irfan.zain@gmail.com>
2020-12-25 15:01:45 +1100

    Added page title

--- src/components/SummaryList.js
+++ src/components/SummaryList.js
@@ -2,11 +2,18 @@ import React from 'react';
 import SummaryCard from './SummaryCard';
 
 export default function SummaryList({ data }) {
-  return Object.keys(data).map((k) => {
-    return (
-      <div className="SummaryList">
-        <SummaryCard k={k} value={data[k]} />
-      </div>
-    )
-  })
+  return (
+    <div>
+      <h1>Worldwide</h1>
+      {
+        Object.keys(data).map((k) => {
+          return (
+            <div className="SummaryList">
+              <SummaryCard k={k} value={data[k]} />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
