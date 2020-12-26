fb990f5ce67cb23d30cabfc62e54c6271be9d65f
Irfan Zainudin <m.irfan.zain@gmail.com>
2020-12-26 19:44:36 +1100

    fix Link tags to point to /covid-tracker as base route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <Link className="NavBar-link" to="/"><b>Home</b></Link>
-      <Link className="NavBar-link" to="/usa"><b>USA</b></Link>
-      <Link className="NavBar-link" to="/malaysia"><b>Malaysia</b></Link>
-      <Link className="NavBar-link" to="/about"><b>About</b></Link>
+      <Link className="NavBar-link" to="/covid-tracker"><b>Home</b></Link>
+      <Link className="NavBar-link" to="/covid-tracker/usa"><b>USA</b></Link>
+      <Link className="NavBar-link" to="/covid-tracker/malaysia"><b>Malaysia</b></Link>
+      <Link className="NavBar-link" to="/covid-tracker/about"><b>About</b></Link>
     </div>
   )
 }
